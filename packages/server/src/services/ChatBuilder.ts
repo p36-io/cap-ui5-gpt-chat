d084afd90eeeb16906108ab3ce2eb0fff7e17b84
mikezaschka <mike.zaschka@p36.io>
2023-02-23 16:37:49 +0100

    refactor: use constants when building the prompt

--- packages/server/src/services/ChatBuilder.ts
+++ packages/server/src/services/ChatBuilder.ts
@@ -1,5 +1,5 @@
 import { Service } from "typedi";
-import { Entity } from "../types/p36.capui5gpt.chat";
+import { Entity, Sender } from "../types/p36.capui5gpt.chat";
 
 @Service()
 export default class ChatBuilder {
@@ -13,7 +13,10 @@ export default class ChatBuilder {
     const messages = await SELECT.from(Entity.Messages).where({ chat_ID: chatId }).orderBy("createdAt");
     return messages
       .map((message) => {
-        return `${message.sender === "AI" ? "AI" : "Human"}: ${message.text.trim().replace(/\n/g, " ")}`;
+        const sender = message.sender === Sender.AI ? Sender.AI : "Human";
+        const plainMessage = message.text.trim().replace(/\n/g, " ");
+
+        return `${sender}: ${plainMessage}`;
       })
       .join("\n");
   }
